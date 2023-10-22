c9c2b494e3ca884227d136424f7bea4a3834128e
Thierry <thierry.lachat@novei.fr>
2023-10-22 19:39:27 +0200

    nav, moi et parcours

--- assets/js/scripts.js
+++
@@ -1,4 +0,0 @@
-window.onscroll = () => {
-    let shadow = Math.floor(scrollY/20)-5;
-    document.querySelector('#aboutMe h2').style.setProperty('--brShadow', `2px ${shadow}px 6px rgba(0,0,0,0.5)`);
-}
