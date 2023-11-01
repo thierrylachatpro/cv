a5ad9b53b8502efd775293ee53ec1ac6c1d39a39
Thierry <thierry.lachat@novei.fr>
2023-11-01 16:40:52 +0100

    ajout du cv papier

--- assets/js/sendmail.js
+++ assets/js/sendmail.js
@@ -1,23 +1,26 @@
-const displayMessage = () => {
-  sentMessage.classList.remove('d-none');
-}
-
 const sendMail = (event) => {
-    event.preventDefault();
+  event.preventDefault();
+
+  try {
+    if (!email.value || !lastname.value || !message.value) {
+      throw new Error('Tous les champs sont obligatoires!');
+    }
 
     Email.send({
-        SecureToken : "8ba29642-d127-4b65-a0f8-3ca5d8956583",
-        To : 'tlachat@gmail.com',
-        From : "contact@thierrylachat.fr",
-        Subject : "Message de "+ lastname.value +" depuis le thierrylachat.fr",
-        Body : message.value.replaceAll('\n','<br>') + '<br><br>'+ email.value
+      SecureToken: "8ba29642-d127-4b65-a0f8-3ca5d8956583",
+      To: 'tlachat@gmail.com',
+      From: "contact@thierrylachat.fr",
+      Subject: "Message de " + lastname.value + " depuis le thierrylachat.fr",
+      Body: message.value.replaceAll('\n', '<br>') + '<br><br>' + email.value
     }).then(
       message => {
-        sentMessage.classList.remove('d-none');
+        sentMessage.innerHTML = 'Merci! Votre message a bien été envoyé!!';
       }
     );
 
-
+  } catch ({name, message}) {
+    sentMessage.innerHTML = message;
+  }
 }
 
-formContact.addEventListener('submit', sendMail);
+sendmail.addEventListener('click', sendMail);
