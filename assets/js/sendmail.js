7bf17d9bea06b581b1ed9f36e23edcb0d6906786
Thierry Lachat <thiery.lachat.pro@gmail.com>
2025-09-02 14:49:16 +0200

    Maj complète

--- assets/js/sendmail.js
+++ assets/js/sendmail.js
@@ -8,8 +8,8 @@ const sendMail = (event) => {
 
     Email.send({
       SecureToken: "8ba29642-d127-4b65-a0f8-3ca5d8956583",
-      To: 'tlachat@gmail.com',
-      From: "contact@thierrylachat.fr",
+      To: 'thierry.lachat.pro@gmail.com',
+      From: "thierry.lachat.pro@gmail.com",
       Subject: "Message de " + lastname.value + " depuis le thierrylachat.fr",
       Body: message.value.replaceAll('\n', '<br>') + '<br><br>' + email.value
     }).then(
